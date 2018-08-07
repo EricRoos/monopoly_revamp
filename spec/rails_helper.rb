da5e6aba239575792b9676465bd4efc69ff5403c
Eric Roos <ericroos13@gmail.com>
2018-08-07 13:34:16 -0500

    move some things around

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,4 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'support/factory_bot'
-require 'support/request_spec_helper.rb'
-require 'support/wait_for_ajax.rb'
 
 
 require 'spec_helper'
@@ -10,6 +7,9 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'support/factory_bot'
+require 'support/request_spec_helper.rb'
+require 'support/wait_for_ajax.rb'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
