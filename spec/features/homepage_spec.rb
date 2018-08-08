03a33f952a4e664f8a7c9496f7a8825fdca4aeda
Eric Roos <ericroos13@gmail.com>
2018-08-07 21:12:49 -0500

    final test

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "Create Game", :type => :feature, js: true do
   let(:password) { 'test123456' }
   scenario "User has no prior games" do
     visit root_path
-    expect(page).to have_content('Monopoly')
+    expect(page).to have_content('Monopoly!')
   end
 end
